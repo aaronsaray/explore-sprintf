437a5f7392bc024e65de913cbdab26d85182a2c1
Aaron Saray <me@aaronsaray.com>
2025-01-16 18:53:46 -0600

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,73 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <wip-message></wip-message>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <main class="max-w-5xl mx-auto px-3 pt-10">
+    <section class="text-center mb-14">
+      <h1 class="antialiased tracking-wide text-5xl font-thin text-center text-gray-100 mb-8">
+        Exploring 
+        <span class="border-b-2 border-orange-700 pb-1">sprintf</span>
+      </h1>
+      <p>
+        Learn the power and potential of <code class="text-gray-300">sprintf()</code> in the browser!
+      </p>
+    </section>
+  
+    <section class="grid grid-cols-2 space-x-12">
+      <div>
+        <fieldset class="mb-6">
+          <legend class="text-gray-400 font-medium mb-2">
+            Format string for <code>sprintf()</code>
+          </legend>
 
-  <main>
-    <TheWelcome />
+          <textarea 
+            v-model="formatString"
+            class="w-full bg-slate-800 focus:bg-slate-950 border border-slate-700 focus:outline-none p-2 placeholder:text-slate-500 text-gray-50" 
+            placeholder="Enter your format string" 
+            rows="1"
+          ></textarea>
+          
+          <div class="text-sm transition-opacity opacity-80 hover:opacity-100">
+            Need an example?
+            <a @click.prevent="example" href="#" class="underline underline-offset-2 text-gray-400">Try this</a>
+          </div>
+        </fieldset>
+
+        <template>
+          <fieldset class="space-y-2">
+            <legend class="text-gray-400 font-medium">Substitutions</legend>
+            <template>
+              <div class="mb-4">
+                <label>
+                  <span class="w-10 inline-block text-gray-400"></span>
+                  <input class="bg-slate-800 focus:bg-slate-950 border border-slate-700 focus:outline-none p-2 placeholder:text-slate-500 text-gray-50" type="text">
+                </label>
+              </div>
+            </template>
+          </fieldset>
+        </template>
+      </div>
+
+      <div>
+        <div class="text-gray-400 font-medium mb-2">
+          Formatted result
+        </div>
+        <div class="bg-slate-950 p-2 drop-shadow">
+          <span class="uppercase text-sm bg-slate-800 px-1 py-0.5 rounded-sm">None</span>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup>
+import { ref } from 'vue';
+
+import WipMessage from './components/WipMessage.vue';
+
+const formatString = ref('');
+
+function example() {
+  formatString.value = 'My string "%s" is here.';
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+</script>
---
+++ src/components/WipMessage.vue
@@ -0,0 +1,32 @@
+<template>
+  <aside class="absolute top-0 right-0 text-sm w-64">
+    <div class="bg-amber-700 text-black p-1 text-sm text-center">
+      This is WIP. 
+      <a @click.prevent="toggle" href="#" class="underline">
+        <span v-if="!showing">Learn more</span>
+        <span v-if="showing">Close</span>
+      </a>
+    </div>
+    <div v-if="showing" class="bg-slate-700 text-gray-100 px-2 py-1 font-light space-y-2">
+      <p class="text-center font-semibold">Work in Progress</p>
+      <p>The following are broken, missing or in progress:</p>
+      <ul class="list-disc list-inside">
+        <li>Any support besides just plain <code>%s</code></li>
+        <li>When you change the format string, all fields reset.</li>
+      </ul>
+      <p>
+        Probably way more things. This is just for fun right now.
+      </p>
+    </div>
+  </aside>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+
+  const showing = ref(false);
+
+  function toggle() {
+    showing.value = !showing.value;
+  }
+</script>
